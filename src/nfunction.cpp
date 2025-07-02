9b17aa87c5e450d9d36f0df6a61b98e84d0b8fae
PiGames <root_4rch@proton.me>
2025-07-02 23:06:07 +0300

    fixed bugs

--- src/nfunction.cpp
+++ src/nfunction.cpp
@@ -6,6 +6,7 @@ using namespace std;
 void noticer(){
 	string noticer;
 	cout << "What would you like to remember?" << endl;
+	cin.ignore();
 	getline(cin, noticer);
 	string cmd = "echo \"echo " + noticer + "\" >> ~/.bashrc";
 	system(cmd.c_str());
--- src/noticpp.cpp
+++ src/noticpp.cpp
@@ -5,7 +5,7 @@
 #include "function.hpp"
 using namespace std;
 int main(){
-	cout << "Welcome to noticpp!" << endl; 
+	cout << "Welcome to noticpp!" << endl;
 	if(getuid() == 0) {
 		cout << "HEY! You can't run this software with root! Would you like to switch normal user(if you are using multi-users except root, don't switch user from here!)?" << endl;
 		cout << "Choose your option: y(es) or n(o)." << endl;
@@ -14,7 +14,7 @@ int main(){
 		switch(rootoption){
 			case 'Y':
 			case 'y':{
-		string command = R"(su - $(awk -F: '$3>=1000 && $3<65534 && $7 ~ /(bash|sh)$/ {print $1; exit}' /etc/passwd))";
+		string command = R"(su - $(awk -F: '$3>=1000 && $3<65534 && $7 ~ /(bash|sh)$/ {print $1; exit}' /etc/passwd) -c '/usr/local/bin/noticpp')";
 		system(command.c_str());
 		noticer();
 			       }break;
@@ -25,7 +25,9 @@ int main(){
 			default:{
 					cout << "Enter a valid option! " << rootoption << " option doesn't exists." << endl;
 				}
+				return 1;
 		}
+		return 0;
 		}else{
 			noticer();
 		}
